4597d34cf8e40836a0cc810e2fbc6f3f8919e081
ardaoz123 <60921183+ardaoz123@users.noreply.github.com>
2022-06-07 15:20:47 +0200

    .

--- Album-Api/Album.Api/Startup.cs
+++ Album-Api/Album.Api/Startup.cs
@@ -45,8 +45,6 @@ namespace Album.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Album.Api v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
